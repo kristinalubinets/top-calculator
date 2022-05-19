6d0b2e98b7148a3772fb041cf78155559e44de83
Kristina Lubinets <kris.lubinec@gmail.com>
2022-05-19 15:16:33 -0400

    Finished essential styles
    Implemented needed styles for index.html file, included new styles for
     dynamic changes after button click.
    
    Next step: creating corresponding elements on action.js file

---
+++ action.js
@@ -0,0 +1,21 @@
+const ac = document.querySelector('.button.ac');
+const division = document.querySelector('.button.action-btn.division');
+const seven = document.querySelector('.button.seven');
+const eight = document.querySelector('.button.eight');
+const nine = document.querySelector('.button.nine');
+const multi = document.querySelector('.button.action-btn.multi');
+const four = document.querySelector('.button.four');
+const five = document.querySelector('.button.five');
+const six = document.querySelector('.button.six');
+const add = document.querySelector('.button.action-btn.add');
+const one = document.querySelector('.button.one');
+const two = document.querySelector('.button.two');
+const three = document.querySelector('.button.three');
+const subtract = document.querySelector('.button.action-btn.subtract');
+const zero = document.querySelector('.button.zero');
+const point = document.querySelector('.button.point');
+const equal = document.querySelector('.button.action-btn.equal');
+
+let resultBox = document.querySelector('.result-box');
+
+
